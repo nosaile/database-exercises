6a35a6547d64a472e26debba02fff2fda4341dbd
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-05-03 15:24:04 -0500

    group by exercise completed

---
+++ group_by_exercises.sql
@@ -0,0 +1,22 @@
+use employees;
+
+
+select distinct title from titles;
+
+select distinct first_name, last_name
+from employees
+where last_name like 'E%'
+  and last_name like '%e';
+
+
+select distinct last_name, count(last_name)
+from employees
+where last_name like '%q%'
+  and last_name not like '%qu%'
+group by last_name;
+
+
+select  distinct gender, count(*)
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+group by gender;
