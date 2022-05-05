b88130d6f1346be49e08c652d072c2e4ce118c91
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-05-05 16:03:02 -0500

    subqueries exercise complete

---
+++ subqueries_exercises.sql
@@ -0,0 +1,29 @@
+use employees;
+
+
+select concat(first_name, ' ', last_name), hire_date
+from employees
+where hire_date in(
+    select hire_date
+    from employees
+    where emp_no = 101010
+    );
+
+
+select title
+from titles
+where emp_no in (
+    select emp_no
+    from employees
+    where employees.employees.first_name = 'Aamod'
+    );
+
+
+select first_name, last_name
+from employees
+where emp_no in (
+    select emp_no
+    from dept_manager
+    where employees.employees.gender = 'F'
+    and to_date > curdate()
+    );
